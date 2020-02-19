f9abb53dd344b9a0692b2b74a74dafcb3ba6ac39
Niek van Agt <niek.van.agt@topic.nl>
2020-02-19 09:40:19 +0100

    Fix 2 more warnings of Linux' checkpatch script

--- dummy-clk.c
+++ dummy-clk.c
@@ -1,20 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Dummy clk driver (to set/enable clocks for testing purposes)
  *
  * Copyright 2020 TOPIC Embedded Products. All rights reserved.
  *
- * This program is free software; you may redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
  */
 
 #include <linux/platform_device.h>
@@ -140,7 +129,9 @@ static int dummy_clk_probe(struct platform_device *pdev)
 	ret = of_property_read_u32_array(pdev->dev.of_node, "clock-frequencies",
 		tmp, data->n_clocks);
 	if (ret) {
-		dev_err(&pdev->dev, "Devicetree clock-frequencies missing for clock %u\n", i);
+		dev_err(&pdev->dev,
+			"Devicetree clock-frequencies missing for clock %u\n",
+			i);
 		return -EINVAL;
 	}
 
